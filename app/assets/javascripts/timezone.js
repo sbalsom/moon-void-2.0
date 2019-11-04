9b3fe4356c05e7747380841d9fbd8a295a0768ed
Sadie Balsom <saralogsin@protonmail.com>
2019-11-04 09:17:21 +0100

    reformats dates with javascript

--- app/assets/javascripts/timezone.js
+++ app/assets/javascripts/timezone.js
@@ -13,3 +13,50 @@ function setTimeZone (offsetHours) {
   }
 
 setTimeZone(offsetHours);
+
+
+
+let lastVoid = document.querySelector('.last-void');
+if (lastVoid != null) {
+  lastVoid = lastVoid.innerText
+  lastVoid = new Date('1970-01-01T' + lastVoid)
+  lastVoid.setHours(lastVoid.getHours() + offsetHours);
+
+}
+
+let nextEnd = document.querySelector('.next-end');
+if (nextEnd != null) {
+  nextEnd = nextEnd.innerText
+  nextEnd = new Date('1970-01-01T' + nextEnd)
+  nextEnd.setHours(nextEnd.getHours() + offsetHours);
+}
+
+let nextVoid = document.querySelector('.next-void')
+if (nextVoid != null) {
+  nextVoid = nextVoid.innerText;
+  nextVoid = new Date('1970-01-01T' + nextVoid)
+  nextVoid.setHours(nextVoid.getHours() + offsetHours);
+}
+
+
+  const lastSlot = document.querySelector('.calculated-last');
+  if (lastSlot != null && lastVoid != null) {
+    let lastHours = (lastVoid.getHours()<10?'0':'') + lastVoid.getHours()
+    let lastMinutes = (lastVoid.getMinutes()<10?'0':'') + lastVoid.getMinutes()
+    lastSlot.innerText = `${lastHours}:${lastMinutes}`
+  }
+
+  const endSlot = document.querySelector('.calculated-end');
+  if (endSlot != null && nextEnd != null) {
+    let endHours = (nextEnd.getHours()<10?'0':'') + nextEnd.getHours()
+    let endMinutes = (nextEnd.getMinutes()<10?'0':'') + nextEnd.getMinutes()
+    endSlot.innerText = `${endHours}:${endMinutes}`
+  }
+ const nextSlot = document.querySelector('.calculated-next');
+  if (nextSlot != null && nextVoid != null) {
+    let nextHours = (nextVoid.getHours()<10?'0':'') + nextVoid.getHours()
+    let nextMinutes = (nextVoid.getMinutes()<10?'0':'') + nextVoid.getMinutes()
+    nextSlot.innerText = `${nextHours}:${nextMinutes}`
+  }
+
+
