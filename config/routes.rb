2e9c16a90f59efdcc33e91a82ab2e2e2008378e7
Sadie Balsom <saralogsin@protonmail.com>
2019-11-04 15:09:19 +0100

    puts authentification back on for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'moons#main'
   require "sidekiq/web"
-  mount Sidekiq::Web => '/sidekiq'
-  # authenticate :user, lambda { |u| u.admin } do
-  #   mount Sidekiq::Web => '/sidekiq'
-  # end
+  # mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
