b7e9c11b91eaa804f7d7fe3f47cd061d2430225c
Sadie Balsom <saralogsin@protonmail.com>
2019-11-04 15:29:57 +0100

    uses staging url for action mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # sidekiq setup
-  config.action_mailer.default_url_options = { host: 'http://www.isthemoonvoid.com/' }
+  config.action_mailer.default_url_options = { host: 'https://warm-gorge-27229.herokuapp.com/' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
