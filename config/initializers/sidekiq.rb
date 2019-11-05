cf6357d42f8de346ef800759b4867b35ef17c246
Sadie Balsom <saralogsin@protonmail.com>
2019-11-05 11:45:41 +0100

    removes commented out code

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -15,10 +15,3 @@ Sidekiq.configure_client do |config|
   end
   config.redis = { namespace:'Moon-Void', url: (ENV["REDISCLOUD_URL"] ||'redis://127.0.0.1:6379/1') }
 end
-
-
-# Sidekiq.configure_server do |config| config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') } end
-
-# Sidekiq.configure_client do |config| config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') } end
-
-# 'redis://127.0.0.1:6379/1'
