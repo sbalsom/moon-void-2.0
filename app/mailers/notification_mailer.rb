b0aaff8dad604a616967f5d945e3530fbb4c57d7
Sadie Balsom <saralogsin@protonmail.com>
2019-11-05 21:56:53 +0100

    expands db report to last two days of creation

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   def report
-    @aspects = Aspect.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day, Date.today.beginning_of_day)
-    @voids = Void.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day, Date.today.beginning_of_day)
+    @aspects = Aspect.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day - 1.days, Date.today.beginning_of_day - 1.days)
+    @voids = Void.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day - 1.days, Date.today.beginning_of_day - 1.days)
     mail(to: 'solarbarmamaise@gmail.com', subject: 'Report on last scraping job')
   end
 end
