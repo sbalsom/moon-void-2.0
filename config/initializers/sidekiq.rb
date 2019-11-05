3d82cef60f5228563419f488a2f81aef04acf93e
Sadie Balsom <saralogsin@protonmail.com>
2019-11-05 11:30:47 +0100

    removes redis url config for now

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,14 +6,14 @@ Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
   end
-  config.redis = { namespace: 'Moon-Void', url: 'redis://127.0.0.1:6379/1' }
+  # config.redis = { namespace: 'Moon-Void', url: 'redis://127.0.0.1:6379/1' }
 end
 
 Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
-  config.redis = { namespace:'Moon-Void', url: 'redis://127.0.0.1:6379/1' }
+  # config.redis = { namespace:'Moon-Void', url: 'redis://127.0.0.1:6379/1' }
 end
 
 
