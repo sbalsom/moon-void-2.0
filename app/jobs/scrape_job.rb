d440e55a8136e3333c67852179ab1edbb988c7b0
Sadie Balsom <saralogsin@protonmail.com>
2019-11-06 13:47:57 +0100

    removes callback from scrape job and modifies email style

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -1,14 +1,14 @@
 class ScrapeJob < ApplicationJob
   queue_as :moon
 
-  after_perform do |job|
-    NotificationMailer.report.deliver_now
-    self.class.set(wait: job.arguments.first).perform_later if job.arguments.first
-  end
+  after_perform :mail_report
 
-  def perform(*wait_time)
+  def perform
     v = VoidScraper.new
     v.find_schedule
   end
-end
 
+  def mail_report
+    NotificationMailer.report.deliver_now
+  end
+end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,6 @@
 class NotificationMailer < ApplicationMailer
   def report
+    @unaspected_voids = Void.where(aspect: nil)
     @aspects = Aspect.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day - 1.days, Date.today.beginning_of_day - 1.days)
     @voids = Void.where('created_at >= ? OR updated_at >= ?', Date.today.beginning_of_day - 1.days, Date.today.beginning_of_day - 1.days)
     mail(to: 'solarbarmamaise@gmail.com', subject: 'Report on last scraping job')
