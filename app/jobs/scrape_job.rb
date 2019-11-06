42ceb096acef55b2508019629602feb86c5d263f
Sadie Balsom <saralogsin@protonmail.com>
2019-11-06 16:01:53 +0100

    trying again with callbacks

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -1,7 +1,11 @@
 class ScrapeJob < ApplicationJob
   queue_as :moon
 
-  after_perform :mail_report
+  after_perform do |job|
+    # invoke another job at your time of choice
+    self.class.set(wait: 1.week).perform_later
+    mail_report
+  end
 
   def perform
     v = VoidScraper.new
