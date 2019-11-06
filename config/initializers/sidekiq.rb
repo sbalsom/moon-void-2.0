978616220e9f4b2461d546fafbc471d17fd9178d
Sadie Balsom <saralogsin@protonmail.com>
2019-11-06 14:09:41 +0100

    adds scheduler for cron jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,12 +6,20 @@ Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
   end
-  config.redis = { namespace: 'Moon-Void', url: (ENV["REDISCLOUD_URL"] ||'redis://127.0.0.1:6379/1') }
+  config.redis = { namespace: 'Moon-Void', url: (ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379/1') }
+  schedule_file = 'config/schedule.yml'
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
-  config.redis = { namespace:'Moon-Void', url: (ENV["REDISCLOUD_URL"] ||'redis://127.0.0.1:6379/1') }
+  config.redis = { namespace: 'Moon-Void', url: (ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379/1') }
+  schedule_file = 'config/schedule.yml'
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'moons#main'
   require "sidekiq/web"
+  require 'sidekiq/cron/web'
   authenticate :user, lambda { |u| u.admin } do
     mount Sidekiq::Web => '/sidekiq'
   end
