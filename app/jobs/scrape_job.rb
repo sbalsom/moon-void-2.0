9d195264f760a35d552b823dd1bd2770cd1d8a40
Sadie Balsom <saralogsin@protonmail.com>
2019-11-06 12:36:10 +0100

    gives the callback flexibility on wait time

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -3,10 +3,10 @@ class ScrapeJob < ApplicationJob
 
   after_perform do |job|
     NotificationMailer.report.deliver_now
-    self.class.set(wait: 1.week).perform_later
+    self.class.set(wait: job.arguments.first).perform_later if job.arguments.first
   end
 
-  def perform
+  def perform(*wait_time)
     v = VoidScraper.new
     v.find_schedule
   end
