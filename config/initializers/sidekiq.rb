90ba57db2e762d4920f53cff7d27ec35acba3d8e
Sadie Balsom <saralogsin@protonmail.com>
2019-11-08 14:32:17 +0100

    disables cron scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,12 +7,12 @@ Sidekiq.configure_server do |config|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 7.days # default
   end
   config.redis = { namespace: 'Moon-Void', url: (ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379/1') }
-  config.on(:startup) do
-    schedule_file = 'config/schedule.yml'
-    if File.exist?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-    end
-  end
+  # config.on(:startup) do
+  #   schedule_file = 'config/schedule.yml'
+  #   if File.exist?(schedule_file)
+  #     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  #   end
+  # end
 end
 
 Sidekiq.configure_client do |config|
@@ -20,10 +20,10 @@ Sidekiq.configure_client do |config|
     chain.add Sidekiq::Status::ClientMiddleware, expiration: 7.days
   end
   config.redis = { namespace: 'Moon-Void', url: (ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379/1') }
-  config.on(:startup) do
-    schedule_file = 'config/schedule.yml'
-    if File.exist?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-    end
-  end
+  # config.on(:startup) do
+  #   schedule_file = 'config/schedule.yml'
+  #   if File.exist?(schedule_file)
+  #     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  #   end
+  # end
 end
