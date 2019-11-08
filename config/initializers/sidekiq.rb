e458ed70ba942933ff6c9b32314ac2ec742815ca
Sadie Balsom <saralogsin@protonmail.com>
2019-11-08 16:46:25 +0100

    setting up procfile for heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,8 @@ require 'sidekiq'
 require 'sidekiq-status'
 require 'sidekiq/web'
 
+$redis = Redis.new
+
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 7.days # default
@@ -14,4 +16,7 @@ Sidekiq.configure_client do |config|
     chain.add Sidekiq::Status::ClientMiddleware, expiration: 7.days
   end
   config.redis = { namespace: 'Moon-Void', url: (ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379/1') }
+  if ENV['REDISCLOUD_URL']
+    $redis = Redis.new(url: ENV['REDISCLOUD_URL'])
+  end
 end
