0c3f2fc8e3df55534164294015f76ad85d320325
Sadie Balsom <saralogsin@protonmail.com>
2019-11-29 12:35:50 +0100

    fixes typo:

--- app/mailers/prune_mailer.rb
+++ app/mailers/prune_mailer.rb
@@ -1,6 +1,6 @@
 class PruneMailer < ApplicationMailer
   def report
-    @earlest_voids = Void.all.order(:end).first(5)
+    @earliest_voids = Void.all.order(:end).first(5)
     @earliest_aspects = Aspect.all.order(:end_void).first(5)
     mail(to: 'solarbarmamaise@gmail.com', subject: 'Report after deleting voids')
   end
