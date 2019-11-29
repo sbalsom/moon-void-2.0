99aff4974d792ea6f1f73010f25e214373c53782
Sadie Balsom <saralogsin@protonmail.com>
2019-11-29 12:08:37 +0100

    adds another job for next morning monthly scraping

--- app/jobs/monthly_scrape_job.rb
+++ app/jobs/monthly_scrape_job.rb
@@ -13,4 +13,5 @@ class MonthlyScrapeJob < ApplicationJob
   end
 end
 
-Sidekiq::Cron::Job.create(name: 'Monthly scrape for voids', cron: '*/30 */2 1 * *', class: 'MonthlyScrapeJob')
+Sidekiq::Cron::Job.create(name: 'Monthly scrape for voids', cron: '*/20 18-23 1 * *', class: 'MonthlyScrapeJob')
+Sidekiq::Cron::Job.create(name: 'Next day monthly scrape', cron: '*/20 0-5 2 * *', class: 'MonthlyScrapeJob')
